68ae2d15956029b92c72371cfb9f14d564efbf31
Tresor Manueno <mandikanda@gmail.com>
2025-11-01 21:26:52 +0200

    Implement Person class and student grading system

--- Task1/Task1.cpp
+++ Task1/Task1.cpp
@@ -1,20 +1,92 @@
-// Task1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+class Person {
+private:
+    string name;
+    string surname;
+    vector<double> homework; 
+    double exam;
+    double finalGrade;
+
+public:
+    
+    Person() {
+        name = "";
+        surname = "";
+        exam = 0;
+        finalGrade = 0;
+    }
+
+
+    Person(string n, string s, vector<double> hw, double ex) {
+        name = n;
+        surname = s;
+        homework = hw;
+        exam = ex;
+        finalGrade = 0;
+    }
+
+
+    void calculateFinalGrade() {
+        double sum = 0;
+  
+        for (double h : homework) {
+            sum += h;
+        }
+
+        double hwAverage;
+        if (homework.size() > 0)
+            hwAverage = sum / homework.size();
+        else
+            hwAverage = 0;
+
+
+        finalGrade = 0.4 * hwAverage + 0.6 * exam;
+    }
+
+ 
+    string getName() { return name; }
+    string getSurname() { return surname; }
+    double getFinalGrade() { return finalGrade; }
+};
+
+int main() {
+
+    int studentNumber;
+	cout << "Enter number of students: ";
+	cin >> studentNumber;
+
+    for (int i = 0; i < studentNumber; i++) {
+        string name, surname;
+        int hwCount;
+        cout << "Enter name of student " << (i + 1) << ": ";
+        cin >> name;
+        cout << "Enter surname of student " << (i + 1) << ": ";
+		cin >> surname;
+
+        cout << "Enter number of homework grades: ";
+        cin >> hwCount;
+        vector<double> homeworkGrades;
+        for (int j = 0; j < hwCount; j++) {
+            double grade;
+            cout << "Enter homework grades "<<j+1<<" :";
+            cin >> grade;
+            homeworkGrades.push_back(grade);
+        }
+
+
+        double examGrade;
+        cout << "Enter exam grade: ";
+        cin >> examGrade;
+        Person student(name, surname, homeworkGrades, examGrade);
+        student.calculateFinalGrade();
+        cout << student.getName() << " " << student.getSurname()
+            << " Final Grade: " << student.getFinalGrade() << endl;
+	}
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
